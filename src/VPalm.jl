a6dece08dfa720efcae5ed1d298d1f9666109160
Rémi Vezy <VEZY@users.noreply.github.com>
2024-03-06 11:12:06 +0100

    Add main dependencies to the project

--- src/VPalm.jl
+++ src/VPalm.jl
@@ -1,5 +1,14 @@
 module VPalm
 
-# Write your package code here.
+# For managing the MTG:
+import MultiScaleTreeGraph
 
+# For the 3D:
+import PlantGeom
+import Meshes
+
+# include("path_to_script")
+
+
+# export my_wonderfull_function
 end
