6efb0624481c60961acb2787052ab079c82aa4e8
Documenter.jl <documenter@juliadocs.github.io>
2024-03-06 09:25:03 +0000

    build based on 0a6d004

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VPalm","category":"page"},{"location":"#VPalm","page":"Home","title":"VPalm","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VPalm.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VPalm]","category":"page"}]
+}
