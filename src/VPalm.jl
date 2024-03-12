f2ab6343b4bc59f7c68056881954d786c603917e
Rémi Vezy <VEZY@users.noreply.github.com>
2024-03-12 15:49:48 +0100

    Add function to read the parameter file as a yaml

--- src/VPalm.jl
+++ src/VPalm.jl
@@ -3,12 +3,14 @@ module VPalm
 # For managing the MTG:
 import MultiScaleTreeGraph
 
+# IO:
+import YAML, OrderedCollections
+
 # For the 3D:
 import PlantGeom
 import Meshes
 
-# include("path_to_script")
+include("IO/read_param_file.jl")
 
-
-# export my_wonderfull_function
+export read_param_file
 end
