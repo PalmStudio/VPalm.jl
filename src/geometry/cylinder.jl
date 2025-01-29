b1887bf59b9d7736d7c12bea55ead4b8f6f363ff
Rémi Vezy <VEZY@users.noreply.github.com>
2025-01-29 23:27:18 +0100

    Update cylinder.jl
    fix cylinder -> was using previous version of Meshes (Point3 instead of Point)

--- src/geometry/cylinder.jl
+++ src/geometry/cylinder.jl
@@ -10,4 +10,4 @@ Returns a normalized cylinder mesh, or a cylinder with radius `r` and length `l`
 - `l`: The length of the cylinder.
 """
 cylinder() = Meshes.CylinderSurface(1.0) |> Meshes.discretize |> Meshes.simplexify
-cylinder(r, l) = Meshes.CylinderSurface(Meshes.Point3(0.0, 0.0, 0.0), Meshes.Point3(0.0, 0.0, l), r) |> Meshes.discretize |> Meshes.simplexify
+cylinder(r, l) = Meshes.CylinderSurface(Meshes.Point(0.0, 0.0, 0.0), Meshes.Point(0.0, 0.0, l), r) |> Meshes.discretize |> Meshes.simplexify
