5d8368fc6e6c97ba0caa8d590829a78d9a7d965e
Rémi Vezy <VEZY@users.noreply.github.com>
2025-01-29 23:28:21 +0100

    Update test-static_mockup.jl
    Total number of phytomers is nb emmited since planting + before planting

--- test/test-static_mockup.jl
+++ test/test-static_mockup.jl
@@ -3,7 +3,7 @@ parameters = read_parameters(file)
 
 @testset "static mockup" begin
     mtg = VPalm.mtg_skeleton(parameters)
-    @test length(mtg) == parameters["nb_leaves_emitted"] * 3 + 2 # nb leaves emitted * 3 (phytomer + internode + leaf) + 2 (stem + plant)
+    @test length(mtg) == (parameters["nb_leaves_emitted"] + parameters["nb_internodes_before_planting"]) * 3 + 2 # nb leaves emitted * 3 (phytomer + internode + leaf) + 2 (stem + plant)
     # Check the length of the mockup: nb leaves emitted * 3 (phytomer + internode + leaf) + 2 (stem + plant)
     @test typeof(mtg) == MultiScaleTreeGraph.Node{MultiScaleTreeGraph.NodeMTG,Dict{Symbol,Any}}
     @test mtg[1][:stem_bending] == 0.0
