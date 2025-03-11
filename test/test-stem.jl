b7ddd87cce61ca31311af8d12324c93ed066e3b7
Rémi Vezy <VEZY@users.noreply.github.com>
2025-03-11 18:40:22 +0100

    Update test-stem.jl

--- test/test-stem.jl
+++ test/test-stem.jl
@@ -9,18 +9,18 @@ end
 
 @testset "stem height" begin
     # Before stem_growth_start:
-    @test VPalm.stem_height(100, parameters["initial_stem_height"], parameters["stem_height_coefficient"], parameters["internode_length_at_maturity"], 120.0, 0.0) == 0.3005242665305668
+    @test VPalm.stem_height(100, parameters["initial_stem_height"], parameters["stem_height_coefficient"], parameters["internode_length_at_maturity"], 120.0, 0.0u"m") == 0.3005242665305668u"m"
 
     # After stem_growth_start:
-    @test VPalm.stem_height(parameters["nb_leaves_emitted"], parameters["initial_stem_height"], parameters["stem_height_coefficient"], parameters["internode_length_at_maturity"], parameters["stem_growth_start"], 0.0) == 1.1801911326167471
+    @test VPalm.stem_height(parameters["nb_leaves_emitted"], parameters["initial_stem_height"], parameters["stem_height_coefficient"], parameters["internode_length_at_maturity"], parameters["stem_growth_start"], 0.0u"m") == 1.1801911326167471u"m"
 end
 
 @testset "stem diameter" begin
-    @test VPalm.stem_diameter(5.0335230597,
+    @test VPalm.stem_diameter(5.0335230597u"m",
         parameters["stem_diameter_max"],
         parameters["stem_diameter_slope"],
         parameters["stem_diameter_inflection"],
-        0.0, # To avoid randomness
+        0.0u"m", # To avoid randomness
         parameters["stem_diameter_snag"]
-    ) == 0.16228677550579518
+    ) == 0.16228677550579518u"m"
 end
